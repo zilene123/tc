d03b692c28a392d34a4d38c24cfb0dfca5c20a6e
zilene123 <145580953+zilene123@users.noreply.github.com>
2024-04-24 16:00:22 -0300

    Update editar.php

--- editar.php
+++ editar.php
@@ -1,7 +1,7 @@
 <?php
 include_once('config.php');
-
 if(isset($_POST['submit'])) {
+
     $id = $_POST['id'];
     $Nome = $_POST['Nome'];
     $Telefone = $_POST['Telefone'];
@@ -10,10 +10,12 @@ if(isset($_POST['submit'])) {
     $Horario = $_POST['Horario'];
 
     $query = "UPDATE cliente SET Nome='$Nome', Telefone='$Telefone', Servico='$Servico', Dia='$Dia', Horario='$Horario' WHERE id=$id";
-    mysqli_query($conexao, $query);
+    
+    mysqli_close($conexao);
 
     // Redirecionamento após a edição do agendamento
     header("Location: most.php");
+
 }
 
 if(isset($_GET['id'])) {
@@ -34,31 +36,67 @@ if(isset($_GET['id'])) {
         h2{
             background-color:#228B22;
         }
-       
+        
     </style>
-
 </head>
 <body>
     <h2>Editar Agendamento</h2>
     <form action="editar.php" method="post">
-        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-        <input type="text" name="Nome" value="<?php echo $row['Nome']; ?>">
-        <input type="text" name="Telefone" value="<?php echo $row['Telefone']; ?>">
-        <select name="Servico">
-            <option value="Barbeiro" <?php if($row['Servico'] == 'Barbeiro') echo 'selected'; ?>>Barbeiro</option>
-            <option value="Cabeleireiro" <?php if($row['Servico'] == 'Cabeleireiro') echo 'selected'; ?>>Cabeleireiro</option>
-            <option value="Manicure" <?php if($row['Servico'] == 'Manicure') echo 'selected'; ?>>Manicure</option>
-            <option value="Spa" <?php if($row['Servico'] == 'Spa') echo 'selected'; ?>>Spa</option>
-        </select>
-        <input type="date" name="Dia" value="<?php echo $row['Dia']; ?>">
-        <input type="time" name="Horario" value="<?php echo $row['Horario']; ?>">
-        <input type="submit" name="submit" value="Salvar">
-        
-        <form action="most.php" method="GET">
-          <button type="submit" name="submit" value="Salvar">Cancelar</button>
-        </form>
+    <fieldset>
+                <legend><b>Fórmulário de agendamentos</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="Nome" id="Nome" class="inputUser" required>
+                    <label for="Nome" class="labelInput">Nome completo</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="Tel" name="Telefone" id="Telefone" class="inputUser" required>
+                    <label for="Telefone" class="labelInput">Telefone</label>
+                </div>
+                <br>
 
-        
+                <label for="Servico">Serviços:</label>
+                <select id="Servico" name="Servico" required>
+                    <option value="">Escolha o serviço</option>
+                    <option value="Barbeiro">Barbeiro</option>
+                    <option value="Cabeleireiro">Cabeleireiro</option>
+                    <option value="Manicure">Manicure</option>
+                    <option value="Spa">Spa</option>
+                </select>
+                <br></br>
+                <label for="Dia">Dia:</label>
+                <input type="date" id="Dia" name="Dia" required>
+                <br><br></br>
+                <label for="Horario">Horário:</label>
+                <input type="time" id="Horario" name="Horario" required>
+                <br></br>
+                <div class="inputBox">
+                    <input type="password" name="Senha" id="Senha" class="inputUser" required>
+                    <label for="Senha" class="labelInput">Senha</label>
+                
+                    
+                    <img src="https://cdn-icons-png.flaticon.com/512/13/13523.png" class="password-toggle-icon" onclick="togglePasswordVisibility()" alt="Mostrar Senha">
+                </div>
+                <script>
+                    function togglePasswordVisibility() {
+                        var passwordInput = document.getElementById("Senha");
+                        var passwordIcon = document.querySelector(".password-toggle-icon");
+
+                        if (passwordInput.type === "password") {
+                            passwordInput.type = "text";
+                            passwordIcon.src = "https://cdn-icons-png.flaticon.com/512/125/125771.png"; //  "olho fechado"
+                        } else {
+                            passwordInput.type = "password";
+                            passwordIcon.src = "https://cdn-icons-png.flaticon.com/512/13/13523.png"; // "olho aberto"
+                        }
+                    }
+                </script>
+                <br></br>
+                <input type="submit" name="submit" id="submit">
+                <a href="most.php" class="btn">Ver horários agendados...</a>
+            </fieldset>
     </form>
+    
 </body>
 </html>
