4e9c24bc11996213947f9b3c169cfec2fb2020a0
zilene123 <145580953+zilene123@users.noreply.github.com>
2024-05-07 12:33:57 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,22 +1,26 @@
 <?php
 include_once('config.php');
 
-if(isset($_GET['id'])) {
+if(isset($_GET['id']) && !empty($_GET['id'])) {
     $id = $_GET['id'];
-    $query = "DELETE FROM cliente WHERE id=$id";
-    mysqli_query($conexao, $query);
+    
+    $query_delete = "DELETE FROM cliente WHERE id = $id";
 
-    header("Location: most.php");
+    $result_delete = mysqli_query($conexao, $query_delete);
+
+    if($result_delete) {
+        echo "<script>alert('Agendamento excluído com sucesso');</script>";
+        echo "<script>window.location = 'most.php';</script>"; 
+    } 
+    else {
+        echo "<script>alert('Erro ao excluir o agendamento');</script>";
+        echo "<script>window.location = 'most.php';</script>"; 
+    }
+} 
+else {
+    echo "<script>alert('ID ou senha do agendamento inválido');</script>";
+    echo "<script>window.location = 'most.php';</script>"; 
 }
+
+mysqli_close($conexao);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-</head>
-<body>
-    <h2>Excluir Agendamento</h2>
-    <p>Tem certeza de que deseja excluir este agendamento?</p>
-    <a href="excluir.php?id=<?php echo $_GET['id']; ?>">Sim</a>
-    <a href="most.php">Não</a>
-</body>
-</html>
