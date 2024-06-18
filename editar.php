de017d87c3d60f6786e5933ad60da540f4ac3a5d
zilene123 <145580953+zilene123@users.noreply.github.com>
2024-06-17 22:13:54 -0300

    Update editar.php

--- editar.php
+++ editar.php
@@ -66,84 +66,190 @@ mysqli_close($conexao);
     <title>Editar Agendamento</title>
 </head>
 <style>
-   body {
-            background-image: url('https://i0.wp.com/revistadecor.com.br/wp-content/uploads/2021/04/ALMA_36_R-scaled.jpg');
-            background-size: cover;
+    * {
+            margin: 0%;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        }
+
+        body {
             font-family: 'Great Vibes', cursive;
+            background-color: #228B22;
+            background-image: url('https://i0.wp.com/revistadecor.com.br/wp-content/uploads/2021/04/ALMA_36_R-scaled.jpg');
+            background-size: cover; 
+            background-position: center; 
+            background-repeat: no-repeat;
+            padding-top: 0;
+            padding-bottom: 20px; 
         }
 
-        h2 {
+
+        .cabeçario {
+            max-width: 1200px;
+            margin: 10px auto;
+            padding: 0 20px;
+        }
+        header {
+            background-color:#228B22;
             color: #fff;
-            background-color:Green;
+            padding: 20px 10px;
         }
-        label{
-            color:#fff;
-            padding: 10px;
-            
-            
+
+        header h1 {
+            margin: 0;
+            font-size: 24px;
         }
+
+        nav ul {
+            list-style-type: none;
+        }
+
+        nav ul li {
+            display: inline;
+            margin-right: 0px;
+        }
+
+        nav ul li a {
+            color: #fff;
+            text-decoration: none;
+        }
+        
+        nav ul li a:hover {
+            background-color: #004d00;
+        }
+        /* Inicio da tabela */
+        .box {
+            color: white;
+            position: absolute;
+            top: 40%;
+            left: 40%; /* espaço lateral */
+            transform: translate(-45%, -45%);
+            background-color: rgba(0, 128, 0, 0.6);
+            padding: 30px;
+            border-radius: 15px;
+            width: 80%;
+        }
+
         fieldset {
-            border: 30px solid Green;
+            border: 3px solid Green;
             border-radius: 10px;
-            padding: 20px;
-            background-color:#5dd55d;
-            
-
-        }
-        input[type="text"],
-        input[type="password"] {
-            border-radius: 20px;
             padding: 10px;
-            
-            border-radius: 20px;
-            margin-bottom: 10px;
-            border: none;
-            font-size: 16px;
         }
 
-        input[type="submit"] {
-            background-color: #008000;
-            color: #fff;
+        legend {
+            border: 1px solid Green;
+            padding: 10px;
+            text-align: center;
+            background-color: Green;
+            border-radius: 8px;
+            color: white;
+        }
+
+        .inputBox {
+            position: relative;
+            margin-bottom: 20px;
+        }
+
+        .inputUser {
+            background: none;
+            border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: 100%;
+            padding: 5px;
+            letter-spacing: 2px;
+        }
+
+        .labelInput {
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
+            color: white;
+        }
+
+        .inputUser:focus ~ .labelInput,
+        .inputUser:valid ~ .labelInput {
+            top: -20px;
+            font-size: 12px;
+            color: while;
+        }
+
+        #submit {
+            background-image: linear-gradient(to right, rgb(0, 80, 0), rgb(0, 128, 0));
+            width: 100%;
+            border: none;
+            padding: 15px;
+            color: white;
+            font-size: 15px;
             cursor: pointer;
-            border: none;
-            border-radius: 20px;
-            padding: 10px 30px;
-            font-size: 16px;
+            border-radius: 10px;
+            margin-top: 20px;
         }
 
-        input[type="submit"]:hover {
-            background-color: #005700;
+        #submit:hover {
+            background-image: linear-gradient(to right, rgb(0, 60, 0), rgb(0, 100, 0));
         }
 
-        select {
-            border-radius: 30px;
-            padding: 10px;
-            margin-bottom: 10px;
-            border: none;
-            font-size: 16px;
+        .btn {
+            display: inline-block;
+            background-image: linear-gradient(to right, rgb(0, 80, 0), rgb(0, 128, 0));
+            padding: 15px;
+            color: white;
+            font-size: 15px;
+            cursor: pointer;
+            border-radius: 10px;
+            text-decoration: none;
+            margin-top: 20px;
+            display: block;
+            text-align: center;
+        }
+
+        .btn:hover {
+            background-image: linear-gradient(to right, rgb(0, 60, 0), rgb(0, 100, 0));
         }
 </style>
 <body>
-    <center> 
+<div class="box">
     <fieldset>
-    <h2>Editar Agendamento</h2>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . '?id=' . $id; ?>" method="post">
-        <label for="nome">Nome:</label><br>
-        <input type="text" id="nome" name="nome" value="<?php echo $nome; ?>"><br>
-        <label for="servico">Serviço:</label><br>
+    <fieldset>
+    <legend><b>Editar informações do agendamento</b></legend><br>
+    
+        <div class="inputBox">
+        <label for="nome" class="labelInput">Nome:</label><br>
+        <input type="text" id="nome" name="nome" class="inputUser" value="<?php echo $nome; ?>"><br>
+        </div>
+
+        <div class="inputBox"> 
+        <label for="servico" class="labelInput">Serviço:</label><br>
         <select id="servico" name="servico">
             <option value="Barbeiro" <?php if($servico == 'Barbeiro') echo 'selected'; ?>>Barbeiro</option>
             <option value="Cabeleireiro" <?php if($servico == 'Cabeleireiro') echo 'selected'; ?>>Cabeleireiro</option>
             <option value="Manicure" <?php if($servico == 'Manicure') echo 'selected'; ?>>Manicure</option>
             <option value="Spa" <?php if($servico == 'Spa') echo 'selected'; ?>>Spa</option>
         </select><br>
-        <label for="dia">Dia:</label><br>
-        <input type="date" id="dia" name="dia" value="<?php echo $dia; ?>"><br>
-        <label for="Horario">Horário:</label><br>
-        <input type="time" id="Horario" name="horario" value="<?php echo $horario; ?>"><br><br>
-        <input type="submit" value="Salvar ">
-    </form>
+        </div>
+        <div class="inputBox">
+        <label for="dia" class="labelInput">Dia:</label><br>
+        <input type="date" id="dia" name="dia" class="inputUser" value="<?php echo $dia; ?>"><br>
+        </div>
+        <div class="inputBox">
+        <label for="Horario" class="labelInput">Horário:</label><br>
+        <input type="time" id="Horario" name="horario" class="inputUser" value="<?php echo $horario; ?>"><br><br>
+        </div>
+        <input type="submit" id="submit" value="Salvar ">
+    
     </fieldset>
-    </center>
+    </form>
 </body>
 </html
