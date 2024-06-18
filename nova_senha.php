6c6384f0ce0477cdc5487d14e7f874f81b1b7a5d
zilene123 <145580953+zilene123@users.noreply.github.com>
2024-06-17 22:18:56 -0300

    Update nova_senha.php

--- nova_senha.php
+++ nova_senha.php
@@ -22,7 +22,7 @@ if(isset($_GET['id']) && !empty($_GET['id'])) {
             // Verifica se a atualização foi bem-sucedida
             if($result_update) {
                 // Redireciona para a página de sucesso após a edição
-                header("Location: senha_edit.php");
+                header("Location: agend_edit.php");
                 exit();
             } else {
                 echo "<script>alert('Erro ao atualizar a senha');</script>";
@@ -45,61 +45,186 @@ mysqli_close($conexao);
     <title>Editar Senha</title>
 </head>
 <style>
-   body {
-        background-image: url('https://i0.wp.com/revistadecor.com.br/wp-content/uploads/2021/04/ALMA_36_R-scaled.jpg');
-        background-size: cover;
-        font-family: 'Great Vibes', cursive;
-        color: #fff;
-    }
+   * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
 
-    h2 {
-        color: #fff;
-        background-color:Green;
-        padding: 10px;
-        border-radius: 10px;
-    }
+        html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        }
 
-    label {
-        color: #fff;
-        padding: 10px;
-    }
+        body {
+            font-family: 'Great Vibes', cursive;
+            background-color: #228B22;
+            background-image: url('https://i0.wp.com/revistadecor.com.br/wp-content/uploads/2021/04/ALMA_36_R-scaled.jpg');
+            background-size: cover; 
+            background-position: center; 
+            background-repeat: no-repeat;
+            padding-top: 0;
+            padding-bottom: 20px; 
+        }
 
-    fieldset {
-        border: 30px solid Green;
-        border-radius: 10px;
-        padding: 20px;
-        background-color:#5dd55d;
-    }
 
-    input[type="password"] {
-        border-radius: 20px;
-        padding: 10px;
-        border-radius: 20px;
-        margin-bottom: 10px;
-        border: none;
-        font-size: 16px;
-    }
+        .cabeçario {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 0 20px;
+        }
+        header {
+            background-color:#228B22;
+            color: #fff;
+            padding: 20px 0px;
+        }
 
-    input[type="submit"] {
-        background-color: #008000;
-        color: #fff;
-        cursor: pointer;
-        border: none;
-        border-radius: 20px;
-        padding: 10px 30px;
-        font-size: 16px;
-    }
+        header h1 {
+            margin: 0;
+            font-size: 24px;
+        }
 
-    input[type="submit"]:hover {
-        background-color: #005700;
-    }
+        nav ul {
+            list-style-type: none;
+        }
+
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+
+        nav ul li a {
+            color: #fff;
+            text-decoration: none;
+        }
+        
+        nav ul li a:hover {
+            background-color: #004d00;
+        }
+        /* Inicio da tabela */
+        .box {
+            color: white;
+            position: absolute;
+            top: 50%;
+            left: 40%;
+            transform: translate(-45%, -45%);
+            background-color: rgba(0, 128, 0, 0.6);
+            padding: 30px;
+            border-radius: 15px;
+            width: 80%;
+        }
+
+        fieldset {
+            border: 3px solid Green;
+            border-radius: 10px;
+            padding: 20px;
+        }
+
+        legend {
+            border: 1px solid Green;
+            padding: 10px;
+            text-align: center;
+            background-color: Green;
+            border-radius: 8px;
+            color: white;
+        }
+
+        .inputBox {
+            position: relative;
+            margin-bottom: 20px;
+        }
+
+        .inputUser {
+            background: none;
+            border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: 100%;
+            padding: 5px;
+            letter-spacing: 2px;
+        }
+
+        .labelInput {
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
+            color: white;
+        }
+
+        .inputUser:focus ~ .labelInput,
+        .inputUser:valid ~ .labelInput {
+            top: -20px;
+            font-size: 12px;
+            color: while;
+        }
+
+        #submit {
+            background-image: linear-gradient(to right, rgb(0, 80, 0), rgb(0, 128, 0));
+            width: 100%;
+            border: none;
+            padding: 15px;
+            color: white;
+            font-size: 15px;
+            cursor: pointer;
+            border-radius: 10px;
+            margin-top: 20px;
+        }
+
+        #submit:hover {
+            background-image: linear-gradient(to right, rgb(0, 60, 0), rgb(0, 100, 0));
+        }
+
+        .btn {
+            display: inline-block;
+            background-image: linear-gradient(to right, rgb(0, 80, 0), rgb(0, 128, 0));
+            padding: 15px;
+            color: white;
+            font-size: 15px;
+            cursor: pointer;
+            border-radius: 10px;
+            text-decoration: none;
+            margin-top: 20px;
+            display: block;
+            text-align: center;
+        }
+
+        .btn:hover {
+            background-image: linear-gradient(to right, rgb(0, 60, 0), rgb(0, 100, 0));
+        }
+        
+        .password-toggle-icon {
+            position: absolute;
+            top: 50%;
+            right: 10px;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
+        .password-toggle-icon {
+            position: absolute;
+            top: 50%;
+            right: 15px;
+            transform: translateY(-50%);
+            cursor: pointer;
+            width: 20px; 
+            height: 20px; 
+            fill: #fff; 
+            transition: transform 0.3s ease; 
+        }
+        .password-toggle-icon:hover {
+            transform: translateY(-50%) scale(1.2); 
+        }
 </style>
 <body>
-    <center> 
-    <fieldset>
-        <h2>Editar Senha</h2>
+<div class="box">
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . '?id=' . $id; ?>" method="post">
-            <div class="inputBox">
+        <fieldset>    
+        <legend><b>Nova senha</b></legend><br>
+        <div class="inputBox">
                 <input type="password" name="novaSenha" id="novaSenha" class="inputUser" required>
                 <label for="novaSenha" class="labelInput">Nova Senha</label>
             </div>
@@ -107,9 +232,9 @@ mysqli_close($conexao);
                 <input type="password" name="confirmarSenha" id="confirmarSenha" class="inputUser" required>
                 <label for="confirmarSenha" class="labelInput">Confirmar Senha</label>
             </div>
-            <input type="submit" value="Salvar">
+            <input type="submit" id="submit" value="Salvar">
         </form>
     </fieldset>
-    </center>
+    
 </body>
 </html>
