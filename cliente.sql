11dee5f72e761954489894e3c1cd0e9e017fdebc
zilene123 <145580953+zilene123@users.noreply.github.com>
2024-06-19 10:17:18 -0300

    Update cliente.sql

--- cliente.sql
+++ cliente.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/05/2024 às 14:33
+-- Tempo de geração: 19/06/2024 às 15:14
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -30,30 +30,38 @@ SET time_zone = "+00:00";
 CREATE TABLE `cliente` (
   `id` int(11) NOT NULL,
   `Nome` varchar(255) NOT NULL,
-  `Telefone` varchar(20) NOT NULL,
-  `Email` varchar(255) DEFAULT NULL,
+  `Telefone` varchar(15) NOT NULL,
+  `Email` varchar(255) NOT NULL,
   `Servico` enum('Barbeiro','Cabeleireiro','Manicure','Spa') NOT NULL,
   `Dia` date NOT NULL,
   `Horario` time NOT NULL,
   `Senha` varchar(255) NOT NULL,
-  `id_funcionario` int(11) DEFAULT NULL
+  `Valor` decimal(10,2) DEFAULT NULL,
+  `Descricao` text DEFAULT NULL,
+  `Status_Atendimento` enum('Cliente Ausente','Atendido') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `cliente`
 --
 
-INSERT INTO `cliente` (`id`, `Nome`, `Telefone`, `Email`, `Servico`, `Dia`, `Horario`, `Senha`, `id_funcionario`) VALUES
-(109, 'Monica Sampaio', '84996121545', 'monisan@gmail.com', 'Spa', '2024-05-09', '14:00:00', 'moni', NULL),
-(110, 'Ruan', '99877332645', 'ruancarlos@gmail.com', 'Spa', '2024-05-08', '17:00:00', 'aaa', NULL),
-(111, 'Flavia', '84991956235', 'car@gmail.com', 'Cabeleireiro', '2024-05-14', '10:00:00', 'aaa', NULL),
-(112, 'Zilene', '95965623', 'zilene@gmail.com', 'Manicure', '2024-05-22', '14:00:00', 'ggggg', NULL),
-(113, 'Zilene', '95965623', 'julicesar@gmail.com', 'Spa', '2024-05-12', '10:00:00', 'hhhh', NULL),
-(114, 'Valdeni de Oliveira', '84991956235', 'carlos@gmail.com', 'Spa', '2024-05-12', '14:00:00', 'aaa', NULL),
-(115, 'Bruna', '8456952315', 'sthefany@gmail.com', 'Cabeleireiro', '2024-05-15', '12:00:00', '123', NULL),
-(116, 'Bruna', '84751265413', 'carlos@gmail.com', 'Manicure', '2024-05-11', '13:00:00', '123456', NULL),
-(117, 'Valdeni de Oliveira', '84751265413', 'carlos@gmail.com', 'Barbeiro', '2024-05-14', '10:00:00', '258', NULL),
-(118, 'Zilene', '8456952315', 'ruancarlos@gmail.com', 'Spa', '2024-05-14', '17:00:00', 'aaa', NULL);
+INSERT INTO `cliente` (`id`, `Nome`, `Telefone`, `Email`, `Servico`, `Dia`, `Horario`, `Senha`, `Valor`, `Descricao`, `Status_Atendimento`) VALUES
+(1, 'Amanda Brazão', '84991956235', 'amanda@gmail.com', 'Spa', '2024-05-17', '15:00:00', 'descançar', 40.00, 'nada a acrecentar', 'Atendido'),
+(3, 'Carmela', '84996121545', 'car@gmail.com', 'Manicure', '2024-05-23', '15:00:00', 'aaa', 100.00, 'Nada a acrecentar', 'Atendido'),
+(4, 'Catia Lima', '84991956235', 'ruancarlos@gmail.com', 'Cabeleireiro', '2024-05-27', '10:00:00', 'aaaaaaa', 0.00, 'Nada a acrecentar', 'Cliente Ausente'),
+(5, 'Carmela', '95965623', 'sthefany@gmail.com', 'Manicure', '2024-05-28', '15:00:00', 'aaa', 50.00, 'Nada a acrecentar', 'Atendido'),
+(6, 'Zida Valentim', '8456952315', 'monisan@gmail.com', 'Manicure', '2024-05-23', '11:40:00', 'aaa', 300.00, 'Adicionou manicure e escova', 'Atendido'),
+(7, 'Carmela', '8456952315', 'ruancarlos@gmail.com', 'Manicure', '2024-05-31', '12:00:00', 'AAA', 120.00, 'Fez tabém uma escova', 'Atendido'),
+(8, 'Ricardo de Oliveira', '84991956235', 'ricardo@gmail.com', 'Manicure', '2024-06-05', '15:00:00', 'rica', 0.00, 'Nada a acrecentar', 'Cliente Ausente'),
+(10, 'Zilene Silva', '84751265413', 'zilene@gmail.com', 'Spa', '2024-07-12', '12:00:00', 'Zilene13#', NULL, NULL, NULL),
+(14, 'Bruna', '95965623', 'carlos@gmail.com', 'Manicure', '2024-06-25', '12:00:00', 'linda', NULL, NULL, NULL),
+(15, 'Paulo Cabral', '8456952315', 'ruancarlos@gmail.com', 'Barbeiro', '2024-06-08', '10:00:00', 'aaa', 30.00, 'Nada a acrecentar', 'Atendido'),
+(17, 'Bruna', '8456952315', 'carlos@gmail.com', 'Cabeleireiro', '2024-06-27', '12:00:00', 'aaa', NULL, NULL, NULL),
+(18, 'Reinaldo de Oliveira', '84751265413', 'oliveira@gmail.com', 'Barbeiro', '2024-06-15', '08:00:00', 'reinaldo23', NULL, NULL, NULL),
+(19, 'Gisele', '6265662326', 'gisele@gmail.com', 'Manicure', '2024-06-19', '14:00:00', '123', NULL, NULL, NULL),
+(20, 'Silva', '646623262', 'silva@gmail.com', 'Cabeleireiro', '2024-06-13', '14:30:00', '456789', NULL, NULL, NULL),
+(21, 'Fravia', '446513262', 'fra@gmail.com', 'Cabeleireiro', '2024-06-14', '10:45:00', '123', NULL, NULL, NULL),
+(22, 'Flavia Marciel', '84994955658', 'fravia@gmail.com', 'Cabeleireiro', '2024-06-18', '13:30:00', 'Fravia13#', NULL, NULL, NULL);
 
 --
 -- Índices para tabelas despejadas
@@ -63,8 +71,7 @@ INSERT INTO `cliente` (`id`, `Nome`, `Telefone`, `Email`, `Servico`, `Dia`, `Hor
 -- Índices de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_funcionario` (`id_funcionario`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -74,17 +81,7 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `cliente`
---
-ALTER TABLE `cliente`
-  ADD CONSTRAINT `fk_funcionario` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionario` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
