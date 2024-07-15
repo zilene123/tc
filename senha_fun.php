116a5bd16257c9c07114442a1bc7ec8e1eaf9148
zilene123 <145580953+zilene123@users.noreply.github.com>
2024-07-15 17:41:45 -0300

    Update senha_fun.php

--- senha_fun.php
+++ senha_fun.php
@@ -38,7 +38,7 @@ if(isset($_GET['id']) && !empty($_GET['id'])) {
 mysqli_close($conexao);
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -46,9 +46,9 @@ mysqli_close($conexao);
 </head>
 <style>
    body {
-        background-image: url('https://i0.wp.com/revistadecor.com.br/wp-content/uploads/2021/04/ALMA_36_R-scaled.jpg');
         background-size: cover;
-        font-family: 'Great Vibes', cursive;
+        background-color:#fff;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         color: #fff;
     }
 
